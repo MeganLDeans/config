16ed1eaa741242b1b26ca3ad78d1223b21f9486d
MeganLDeans <megan.deans@snhu.edu>
2022-10-13 13:11:36 -0400

    initial commit

---
+++ pages/urls.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python 
+__author__ = "Megan Deans"
+
+from django.urls import path
+from .views import HomePageView
+from.views import AboutPageView
+
+urlpatterns = [
+path("about/", AboutPageView.as_view(), name="about"),
+path("", HomePageView.as_view(), name="home"),
+]
